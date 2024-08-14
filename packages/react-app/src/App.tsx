2a1ffd668f01dd0326755806f28ce229017530ef
Prashants23 <prashant23shukla@gmail.com>
2024-08-14 19:41:17 +0530

    initialization: monorepo and react app initialiozation

---
+++ packages/react-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {DUMMY_CONSTANT} from 'shared/src/utils/index'
+
+function App() {
+  return (
+    <div className="App">
+     {DUMMY_CONSTANT}
+    </div>
+  );
+}
+
+export default App;
